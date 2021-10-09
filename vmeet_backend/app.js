c1f55f30af6873500cf8814cefba1084338870c6
Nithin-Reddy-02 <jonnynithin7@gmail.com>
2021-10-09 23:10:16 +0530

    added video conf

--- vmeet_backend/app.js
+++ vmeet_backend/app.js
@@ -1,36 +1,39 @@
-const express = require('express');
-const http = require('http');
-let cors = require('cors');
-const app = express();
-const bodyParser = require('body-parser');
-const path = require("path");
-let xss = require("xss");
-const mongoose = require('mongoose');
+const express = require('express')
+const http = require('http')
+let cors = require('cors')
+const app = express()
+const bodyParser = require('body-parser')
+const path = require('path')
+let xss = require('xss')
+const mongoose = require('mongoose')
 
-let server = http.createServer(app);
+let server = http.createServer(app)
 let io = require('socket.io')(server)
-mongoose.connect(process.env.VMEET_DB_CONN_STRING,{ useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
-    console.log('DB Connected');
+mongoose
+  .connect(process.env.VMEET_DB_CONN_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connected')
   })
   .catch((err) => {
-    console.log('Error connecting to muxdb' + err.stack);
-});
+    console.log('Error connecting to muxdb' + err.stack)
+  })
 
+app.use(cors())
+app.use(bodyParser.json())
 
-app.use(cors());
-app.use(bodyParser.json());
-
-app.get('/',(req,res)=>{
-    return res.send("connected!");
+app.get('/', (req, res) => {
+  return res.send('connected!')
 })
-var auth = express.Router();
-require('./src/routes/auth.js')(auth);
-app.use('/auth',auth);
-app.set('port',(process.env.PORT || 8000));
-
+var auth = express.Router()
+require('./src/routes/auth.js')(auth)
+app.use('/auth', auth)
+app.set('port', process.env.PORT || 8000)
 
 sanitizeString = (str) => {
-	return xss(str)
+  return xss(str)
 }
 
 connections = {}
@@ -38,87 +41,100 @@ messages = {}
 timeOnline = {}
 
 io.on('connection', (socket) => {
+  socket.on('join-call', (path) => {
+    if (connections[path] === undefined) {
+      connections[path] = []
+    }
+    connections[path].push(socket.id)
 
-	socket.on('join-call', (path) => {
-		if(connections[path] === undefined){
-			connections[path] = []
-		}
-		connections[path].push(socket.id)
+    timeOnline[socket.id] = new Date()
 
-		timeOnline[socket.id] = new Date()
+    for (let a = 0; a < connections[path].length; ++a) {
+      io.to(connections[path][a]).emit(
+        'user-joined',
+        socket.id,
+        connections[path]
+      )
+    }
 
-		for(let a = 0; a < connections[path].length; ++a){
-			io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
-		}
+    if (messages[path] !== undefined) {
+      for (let a = 0; a < messages[path].length; ++a) {
+        io.to(socket.id).emit(
+          'chat-message',
+          messages[path][a]['data'],
+          messages[path][a]['sender'],
+          messages[path][a]['socket-id-sender']
+        )
+      }
+    }
 
-		if(messages[path] !== undefined){
-			for(let a = 0; a < messages[path].length; ++a){
-				io.to(socket.id).emit("chat-message", messages[path][a]['data'], 
-					messages[path][a]['sender'], messages[path][a]['socket-id-sender'])
-			}
-		}
+    console.log(path, connections[path])
+  })
 
-		console.log(path, connections[path])
-	})
+  socket.on('signal', (toId, message) => {
+    io.to(toId).emit('signal', socket.id, message)
+  })
 
-	socket.on('signal', (toId, message) => {
-		io.to(toId).emit('signal', socket.id, message)
-	})
+  socket.on('chat-message', (data, sender) => {
+    data = sanitizeString(data)
+    sender = sanitizeString(sender)
 
-	socket.on('chat-message', (data, sender) => {
-		data = sanitizeString(data)
-		sender = sanitizeString(sender)
+    var key
+    var ok = false
+    for (const [k, v] of Object.entries(connections)) {
+      for (let a = 0; a < v.length; ++a) {
+        if (v[a] === socket.id) {
+          key = k
+          ok = true
+        }
+      }
+    }
 
-		var key
-		var ok = false
-		for (const [k, v] of Object.entries(connections)) {
-			for(let a = 0; a < v.length; ++a){
-				if(v[a] === socket.id){
-					key = k
-					ok = true
-				}
-			}
-		}
+    if (ok === true) {
+      if (messages[key] === undefined) {
+        messages[key] = []
+      }
+      messages[key].push({
+        sender: sender,
+        data: data,
+        'socket-id-sender': socket.id,
+      })
+      console.log('message', key, ':', sender, data)
 
-		if(ok === true){
-			if(messages[key] === undefined){
-				messages[key] = []
-			}
-			messages[key].push({"sender": sender, "data": data, "socket-id-sender": socket.id})
-			console.log("message", key, ":", sender, data)
+      for (let a = 0; a < connections[key].length; ++a) {
+        io.to(connections[key][a]).emit('chat-message', data, sender, socket.id)
+      }
+    }
+  })
 
-			for(let a = 0; a < connections[key].length; ++a){
-				io.to(connections[key][a]).emit("chat-message", data, sender, socket.id)
-			}
-		}
-	})
+  socket.on('disconnect', () => {
+    var diffTime = Math.abs(timeOnline[socket.id] - new Date())
+    var key
+    for (const [k, v] of JSON.parse(
+      JSON.stringify(Object.entries(connections))
+    )) {
+      for (let a = 0; a < v.length; ++a) {
+        if (v[a] === socket.id) {
+          key = k
 
-	socket.on('disconnect', () => {
-		var diffTime = Math.abs(timeOnline[socket.id] - new Date())
-		var key
-		for (const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))) {
-			for(let a = 0; a < v.length; ++a){
-				if(v[a] === socket.id){
-					key = k
+          for (let a = 0; a < connections[key].length; ++a) {
+            io.to(connections[key][a]).emit('user-left', socket.id)
+          }
 
-					for(let a = 0; a < connections[key].length; ++a){
-						io.to(connections[key][a]).emit("user-left", socket.id)
-					}
-			
-					var index = connections[key].indexOf(socket.id)
-					connections[key].splice(index, 1)
+          var index = connections[key].indexOf(socket.id)
+          connections[key].splice(index, 1)
 
-					console.log(key, socket.id, Math.ceil(diffTime / 1000))
+          console.log(key, socket.id, Math.ceil(diffTime / 1000))
 
-					if(connections[key].length === 0){
-						delete connections[key]
-					}
-				}
-			}
-		}
-	})
+          if (connections[key].length === 0) {
+            delete connections[key]
+          }
+        }
+      }
+    }
+  })
 })
 
-server.listen(app.get('port'),()=>{
-    console.log('server listening on port ',app.get('port'));
+server.listen(app.get('port'), () => {
+  console.log('server listening on port ', app.get('port'))
 })
---
+++ vmeet_frontend/src/components/verification.js
@@ -0,0 +1,69 @@
+import React, { useRef } from 'react'
+import Webcam from 'react-webcam'
+import axios from 'axios'
+
+function Verification() {
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: 'user',
+  }
+  const webcamRef = useRef(null)
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot()
+    console.log(imageSrc)
+    call(imageSrc)
+  }
+
+  const postDetails = async (image) => {
+    const data = new FormData()
+    data.append('file', image)
+    data.append('upload_preset', 'Instafam')
+    data.append('cloud_name', 'abcd1234huy')
+    let response = await axios.post(
+      'https://api.cloudinary.com/v1_1/abcd1234huy/image/upload',
+      data
+    )
+    return response.data.url
+  }
+
+  async function call(image) {
+    const imgSrc = await postDetails(image)
+    let value = await axios.post(
+      'http://ec2-18-188-117-109.us-east-2.compute.amazonaws.com/api/distance',
+      {
+        first: imgSrc,
+        second:
+          'https://pbs.twimg.com/profile_images/669103856106668033/UF3cgUk4_400x400.jpg',
+      }
+    )
+
+    if (parseFloat(value.data.distance) > 0.7) {
+      window.location.href = '/abcd'
+    } else {
+      console.log('flase')
+    }
+  }
+
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <h1> Capture your video for verification </h1>
+        <Webcam
+          audio={false}
+          height={300}
+          ref={webcamRef}
+          screenshotFormat='image/jpeg'
+          width={300}
+          videoConstraints={videoConstraints}
+        />
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        <button onClick={capture}>Capture</button>
+      </div>
+    </>
+  )
+}
+
+export default Verification
