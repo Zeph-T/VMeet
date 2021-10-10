c34d5f8c30792ae8304d08d6f10c4c8b6fea4063
Somya S. Singh <somu12.ss@gmail.com>
2021-10-10 11:26:53 +0530

    connections extablished

--- vmeet_frontend/src/components/Signin.js
+++ vmeet_frontend/src/components/Signin.js
@@ -9,141 +9,198 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import WrappedButton from "./common/WrappedButton";
 import Grid from '@material-ui/core/Grid';
 import Switch from '@material-ui/core/Switch';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import { FormGroup } from "@material-ui/core";
+
+const IOSSwitch = styled((props) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+        padding: 0,
+        margin: 2,
+        transitionDuration: '300ms',
+        '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+                backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+                opacity: 1,
+                border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+                opacity: 0.5,
+            },
+        },
+        '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#33cf4d',
+            border: '6px solid #fff',
+        },
+        '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+                theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[600],
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+        },
+    },
+    '& .MuiSwitch-thumb': {
+        boxSizing: 'border-box',
+        width: 22,
+        height: 22,
+    },
+    '& .MuiSwitch-track': {
+        borderRadius: 26 / 2,
+        backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+        opacity: 1,
+        transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+        }),
+    },
+}));
 
 
 const SignIn = () => {
-  const { state, dispatch } = useContext(UserContext);
-  const history = useHistory();
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [loginIsInProgress, setLoginIsInProgress] = useState(false);
-  const PostData = () => {
-    if (
-      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        email
-      )
-    ) {
-      M.toast({ html: "invalid Email", classes: "#43a047 red darken-1" });
-      return;
-    }
-    setLoginIsInProgress(true);
-    if(isAdmin){
-        axios
-        .post(api.BASE_URL + api.FACULTY_LOGIN_URL, {
-          email: email,
-          password: password,
-        })
-        .then((data) => {
-          if (data.data.error) {
-            M.toast({ html: data.error, classes: "#e53935 red darken-1" });
-          } else {
-            localStorage.setItem("AccessToken", data.data.authToken);
-            dispatch({ type: "USER", payload: data.data });
-            M.toast({
-              html: "signed in successfully",
-              classes: "#43a047 green darken-1",
-            });
-            history.push("/");
-          }
-          setLoginIsInProgress(false);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }else{
-        axios
-      .post(api.BASE_URL + api.STUDENT_LOGIN_URL, {
-        email: email,
-        password: password,
-      })
-      .then((data) => {
-        if (data.data.error) {
-          M.toast({ html: data.error, classes: "#e53935 red darken-1" });
-        } else {
-          localStorage.setItem("AccessToken", data.data.authToken);
-          dispatch({ type: "USER", payload: data.data });
-          M.toast({
-            html: "signed in successfully",
-            classes: "#43a047 green darken-1",
-          });
-          history.push("/");
+    const { state, dispatch } = useContext(UserContext);
+    const history = useHistory();
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [loginIsInProgress, setLoginIsInProgress] = useState(false);
+    const PostData = () => {
+        if (
+            !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+                email
+            )
+        ) {
+            M.toast({ html: "invalid Email", classes: "#43a047 red darken-1" });
+            return;
         }
-        setLoginIsInProgress(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    }
-  };
-  return (
-    <div
-      style={{ display: "flex", justifyContent: "center", margin: "2rem 0rem" }}
-    >
-      <div className="mycard">
-        <div className="card auth-card input-field">
-          <h2 className="instafamheading">V-meet</h2>
-          <input
-            type="text"
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Grid
-            item
-            xs={12}
-            md={12}
-            spacing={1}
-            style={{ display: "flex", flexDirection: "row" }}
-          >
-            <Grid item style={{ margin: "1rem" }}>
-              {" "}
-              Student
-            </Grid>
-            <FormControlLabel
-              control={
-                <Switch
-                  size="small"
-                  color="none"
-                  onChange={() => setIsAdmin(!isAdmin)}
-                  name="isAdmin"
-                />
-              }
+        setLoginIsInProgress(true);
+        if (isAdmin) {
+            axios
+                .post(api.BASE_URL + api.FACULTY_LOGIN_URL, {
+                    email: email,
+                    password: password,
+                })
+                .then((data) => {
+                    if (data.data.error) {
+                        M.toast({ html: data.error, classes: "#e53935 red darken-1" });
+                    } else {
+                        localStorage.setItem("AccessToken", data.data.authToken);
+                        dispatch({ type: "USER", payload: data.data });
+                        M.toast({
+                            html: "signed in successfully",
+                            classes: "#43a047 green darken-1",
+                        });
+                        history.push("/");
+                    }
+                    setLoginIsInProgress(false);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        } else {
+            axios
+                .post(api.BASE_URL + api.STUDENT_LOGIN_URL, {
+                    email: email,
+                    password: password,
+                })
+                .then((data) => {
+                    if (data.data.error) {
+                        M.toast({ html: data.error, classes: "#e53935 red darken-1" });
+                    } else {
+                        localStorage.setItem("AccessToken", data.data.authToken);
+                        dispatch({ type: "USER", payload: data.data });
+                        M.toast({
+                            html: "signed in successfully",
+                            classes: "#43a047 green darken-1",
+                        });
+                        history.push("/");
+                    }
+                    setLoginIsInProgress(false);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    };
+    return (
+        <div
+            style={{ display: "flex", justifyContent: "space-around", padding: '5rem' }}
+        >
+            <div className="mycard">
+                <div className="card auth-card input-field col s12 m6">
+                    <h2 className="instafamheading" style={{ margin: '0 6.5rem' }}>V-meet</h2>
+
+                    <div class="input-field col s12" style={{ margin: '0, 3rem' }} >
+                        <input
+                            type="text"
+                            value={email}
+                            style={{ color: '#091353', fontWeight: '600', }}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label for="email" style={{ color: '#091353', fontWeight: '600' }}>Email</label>
+                    </div>
+
+                    <div class="input-field col s6">
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <label for="password" style={{ color: '#091353', fontWeight: '600' }}>Password</label>
+                    </div>
+
+
+                    <Grid
+                        item
+                        xs={12}
+                        md={12}
+                        spacing={1}
+                        style={{ display: "flex", flexDirection: "row" }}
+                    >
+                        <Grid item style={{ margin: "1rem" }}>
+                            {" "}
+                            Student
+                        </Grid>
+                        <FormControlLabel
+                            control={<IOSSwitch sx={{ m: 1 }} />}
+                            onChange={() => setIsAdmin(!isAdmin)}
+                            name='isAdmin'
+                        />
+                        <Grid item style={{ marginTop: "1rem" }}>
+                            Faculty
+                        </Grid>
+                    </Grid>
+                    <WrappedButton
+                        key="loginButton"
+                        buttonKey="loginButton"
+                        disabled={loginIsInProgress}
+                        variant="contained"
+                        onClick={PostData}
+                        name="Login"
+                        size="large"
+                        style={{ width: "100%", padding: "0.8rem", backgroundColor: '#081044', color: 'white', fontWeight: '600', }}
+                        icon=""
+                    />
+                    <h6>
+                        <Link style={{ color: '#081044' }} to="/signup">Don't have an account ?</Link>
+                    </h6>
+                </div>
+            </div>
+            <img
+                src={ReactLogo}
+                alt="React Logo"
+                style={{ width: "600px", height: "auto", margin: "3rem 1rem", padding: '2rem' }}
             />
-            <Grid item style={{ marginTop: "1rem" }}>
-              Faculty
-            </Grid>
-          </Grid>
-          <WrappedButton
-            key="loginButton"
-            buttonKey="loginButton"
-            disabled={loginIsInProgress}
-            variant="contained"
-            color="primary"
-            onClick={PostData}
-            name="Login"
-            size="large"
-            style={{ width: "100%", padding: "0.8rem" }}
-            icon=""
-          />
-          <h5>
-            <Link to="/signup">Don't have an account ?</Link>
-          </h5>
         </div>
-      </div>
-      <img
-        src={ReactLogo}
-        alt="React Logo"
-        style={{ width: "400px", height: "auto", margin: "3rem 5rem" }}
-      />
-    </div>
-  );
+    );
 };
 
 export default SignIn;
--- vmeet_frontend/src/components/Signup.js
+++ vmeet_frontend/src/components/Signup.js
@@ -1,12 +1,64 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import M from 'materialize-css'
 import { api } from '../utilities';
-import {options} from '../utilities'
+import { options } from '../utilities'
 import Grid from '@material-ui/core/Grid';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import ReactLogo from "../svgs/homePage.svg";
+import { styled } from '@mui/material/styles';
 
+const IOSSwitch = styled((props) => (
+    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+    width: 42,
+    height: 26,
+    padding: 0,
+    '& .MuiSwitch-switchBase': {
+        padding: 0,
+        margin: 2,
+        transitionDuration: '300ms',
+        '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+                backgroundColor: theme.palette.mode === 'dark' ? '#2ECA45' : '#65C466',
+                opacity: 1,
+                border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+                opacity: 0.5,
+            },
+        },
+        '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#33cf4d',
+            border: '6px solid #fff',
+        },
+        '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+                theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[600],
+        },
+        '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+        },
+    },
+    '& .MuiSwitch-thumb': {
+        boxSizing: 'border-box',
+        width: 22,
+        height: 22,
+    },
+    '& .MuiSwitch-track': {
+        borderRadius: 26 / 2,
+        backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
+        opacity: 1,
+        transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+        }),
+    },
+}));
 
 const Signup = () => {
     const history = useHistory()
@@ -16,7 +68,7 @@ const Signup = () => {
     const [image, setImage] = useState("")
     const urlRef = useRef(null);
     const [isAdmin, setIsAdmin] = useState(false);
-   
+
 
     const uploadProfilepic = () => {
         const data = new FormData()
@@ -42,7 +94,7 @@ const Signup = () => {
             M.toast({ html: "invalid Email", classes: "#43a047 red darken-1" })
             return
         }
-        if(isAdmin){
+        if (isAdmin) {
             fetch(api.BASE_URL + api.FACULTY_SIGNUP_URL, {
                 method: "post",
                 headers: options,
@@ -65,7 +117,7 @@ const Signup = () => {
                     console.log(err)
                 })
         }
-        else{
+        else {
             fetch(api.BASE_URL + api.STUDENT_SIGNUP_URL, {
                 method: "post",
                 headers: options,
@@ -88,7 +140,7 @@ const Signup = () => {
                     console.log(err)
                 })
         }
-      
+
     }
 
     const PostData = () => {
@@ -100,27 +152,40 @@ const Signup = () => {
 
     }
     return (
-        <div className="mycard">
-            <div className="card auth-card input-field">
-                <h2 className="instafamheading">V-meet</h2>
+        <div style={{ display: "flex", justifyContent: "space-around", padding: '5rem'}}>
+            <div className="mycard" >
+                <div className="card auth-card input-field">
+                    <h2 className="instafamheading" style={{ margin: '0 6.5rem' }}>V-meet</h2>
+                <div class="input-field col s12" style={{ margin: '0, 3rem' }} >
+                    <input
+                        type='text'
+                        value={name}
+                        style={{ color: '#091353', fontWeight: '600', }}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <label for="text" style={{ color: '#091353', fontWeight: '600' }}>Name</label>
+                </div>
+
+                <div class="input-field col s12" style={{ margin: '0, 3rem' }} >
                 <input
                     type='text'
-                    placeholder='name'
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <input
-                    type='text'
-                    placeholder='email'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
+                    <label for="text" style={{ color: '#091353', fontWeight: '600' }}>Email</label>
+                </div>
+
+                <div class="input-field col s12" style={{ margin: '0, 3rem' }} >
                 <input
                     type='text'
-                    placeholder='password'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
+                    <label for="text" style={{ color: '#091353', fontWeight: '600' }}>Password</label>
+                </div>
+
+               
+                
                 <Grid
                     item
                     xs={12}
@@ -128,21 +193,16 @@ const Signup = () => {
                     spacing={1}
                     style={{ display: "flex", flexDirection: "row" }}
                 >
-                    <Grid item style={{ margin: "1rem" }}>
+                    <Grid item style={{ margin: "1rem",color:'#091353' }}>
                         {" "}
                         Student
                     </Grid>
                     <FormControlLabel
-                        control={
-                            <Switch
-                                size="small"
-                                color="none"
-                                onChange={() => setIsAdmin(!isAdmin)}
-                                name="isAdmin"
-                            />
-                        }
-                    />
-                    <Grid item style={{ marginTop: "1rem" }}>
+                            control={<IOSSwitch sx={{ m: 1 }} />}
+                            onChange={() => setIsAdmin(!isAdmin)}
+                            name='isAdmin'
+                        />
+                    <Grid item style={{ marginTop: "1rem",color:'#091353'  }}>
                         Faculty
                     </Grid>
                 </Grid>
@@ -158,21 +218,26 @@ const Signup = () => {
                     </div>
                 </div>
 
-                <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                <button className="btn waves-effect waves-light #64b5f6 "
                     onClick={() => PostData()}
+                    style={{ width: "100%", backgroundColor: '#081044', color: 'white', fontWeight: '600', }}
                 >
                     SignUp
                 </button>
                 <h5>
-                    <Link to="/signin">Already have an account ?</Link>
+                    <Link to="/signin" style={{ color: '#081044' }}>Already have an account ?</Link>
                 </h5>
-
-
 
             </div>
         </div>
-    )
-
-}
+        <img
+        src={ReactLogo}
+        alt="React Logo"
+        style={{ width: "600px", height: "auto", margin: " 1rem",padding:'2rem' }}
+      />
+      </div>
+       
+    );
+};
 
 export default Signup
